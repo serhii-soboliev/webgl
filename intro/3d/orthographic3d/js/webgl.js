c3ba6111a76427e7700adec07059b04df5473ad4
SerhiiSoboliev <sergiy.sobolev@gmail.com>
2020-01-13 15:54:42 +0200

    3D F

--- intro/3d/orthographic3d/js/webgl.js
+++ intro/3d/orthographic3d/js/webgl.js
@@ -1,36 +1,23 @@
 "use strict";
 
 function main() {
-  // Get A WebGL context
-  /** @type {HTMLCanvasElement} */
   var canvas = document.getElementById("canvas");
   var gl = canvas.getContext("webgl");
   if (!gl) {
     return;
   }
 
-  // setup GLSL program
   var program = webglUtils.createProgramFromScripts(gl, ["3d-vertex-shader", "3d-fragment-shader"]);
-
-  // look up where the vertex data needs to go.
   var positionLocation = gl.getAttribLocation(program, "a_position");
   var colorLocation = gl.getAttribLocation(program, "a_color");
-
-  // lookup uniforms
   var matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
-  // Create a buffer to put positions in
   var positionBuffer = gl.createBuffer();
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  // Put geometry data into buffer
   setGeometry(gl);
 
-  // Create a buffer to put colors in
   var colorBuffer = gl.createBuffer();
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = colorBuffer)
   gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  // Put geometry data into buffer
   setColors(gl);
 
   function radToDeg(r) {
@@ -47,7 +34,6 @@ function main() {
 
   drawScene();
 
-  // Setup a ui.
   webglLessonsUI.setupSlider("#x", {value: translation[0], slide: updatePosition(0), max: gl.canvas.width });
   webglLessonsUI.setupSlider("#y", {value: translation[1], slide: updatePosition(1), max: gl.canvas.height});
   webglLessonsUI.setupSlider("#z", {value: translation[2], slide: updatePosition(2), max: gl.canvas.height});
@@ -81,28 +67,15 @@ function main() {
     };
   }
 
-  // Draw the scene.
   function drawScene() {
     webglUtils.resizeCanvasToDisplaySize(gl.canvas);
-
-    // Tell WebGL how to convert from clip space to pixels
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-    // Clear the canvas.
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
     gl.enable(gl.CULL_FACE);
-
-    // Tell it to use our program (pair of shaders)
+    gl.enable(gl.DEPTH_TEST);
     gl.useProgram(program);
-
-    // Turn on the position attribute
     gl.enableVertexAttribArray(positionLocation);
-
-    // Bind the position buffer.
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-    // Tell the position attribute how to get data out of positionBuffer (ARRAY_BUFFER)
     var size = 3;          // 3 components per iteration
     var type = gl.FLOAT;   // the data is 32bit floats
     var normalize = false; // don't normalize the data
@@ -110,34 +83,21 @@ function main() {
     var offset = 0;        // start at the beginning of the buffer
     gl.vertexAttribPointer(
         positionLocation, size, type, normalize, stride, offset);
-
-    // Turn on the color attribute
     gl.enableVertexAttribArray(colorLocation);
-
-    // Bind the color buffer.
     gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-
-    // Tell the attribute how to get data out of colorBuffer (ARRAY_BUFFER)
     var size = 3;                 // 3 components per iteration
     var type = gl.UNSIGNED_BYTE;  // the data is 8bit unsigned values
     var normalize = true;         // normalize the data (convert from 0-255 to 0-1)
     var stride = 0;               // 0 = move forward size * sizeof(type) each iteration to get the next position
     var offset = 0;               // start at the beginning of the buffer
-    gl.vertexAttribPointer(
-        colorLocation, size, type, normalize, stride, offset);
-
-    // Compute the matrices
+    gl.vertexAttribPointer(colorLocation, size, type, normalize, stride, offset);
     var matrix = m4.projection(gl.canvas.clientWidth, gl.canvas.clientHeight, 400);
     matrix = m4.translate(matrix, translation[0], translation[1], translation[2]);
     matrix = m4.xRotate(matrix, rotation[0]);
     matrix = m4.yRotate(matrix, rotation[1]);
     matrix = m4.zRotate(matrix, rotation[2]);
     matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
-
-    // Set the matrix.
     gl.uniformMatrix4fv(matrixLocation, false, matrix);
-
-    // Draw the geometry.
     var primitiveType = gl.TRIANGLES;
     var offset = 0;
     var count = 16 * 6;
@@ -148,7 +108,6 @@ function main() {
 var m4 = {
 
   projection: function(width, height, depth) {
-    // Note: This matrix flips the Y axis so 0 is at the top.
     return [
        2 / width, 0, 0, 0,
        0, -2 / height, 0, 0,
@@ -286,7 +245,6 @@ var m4 = {
 
 };
 
-// Fill the buffer with the values that define a letter 'F'.
 function setGeometry(gl) {
   gl.bufferData(
       gl.ARRAY_BUFFER,
@@ -421,7 +379,6 @@ function setGeometry(gl) {
       gl.STATIC_DRAW);
 }
 
-// Fill the buffer with colors for the 'F'.
 function setColors(gl) {
   gl.bufferData(
       gl.ARRAY_BUFFER,
